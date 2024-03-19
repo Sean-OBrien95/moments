e0fe5fa37f6235515dc856e8df385664910a08a3
Seán O'Brien <sean95ob@gmail.com>
2024-03-19 17:36:51 +0000

    Prepare deploy

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 axios.defaults.baseURL = "https://drf-api11-bdfdb88f585f.herokuapp.com/";
+// axios.defaults.baseURL = "https://8000-seanobrien95-drfapi-vpqqp4zhbuj.ws-eu110.gitpod.io";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
